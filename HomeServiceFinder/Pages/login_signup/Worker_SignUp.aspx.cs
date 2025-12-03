fdd6862ae2ece7df855c236e4e26c3e4b2c3fa6c
anujm <anujmehta1106@gmail.com>
2025-12-03 11:17:23 +0530

    anuj

--- HomeServiceFinder/Pages/login_signup/Worker_SignUp.aspx.cs
+++ HomeServiceFinder/Pages/login_signup/Worker_SignUp.aspx.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Xml.Linq;
 
 namespace HomeServiceFinder.Pages.login_signup
 {
@@ -14,11 +15,65 @@ namespace HomeServiceFinder.Pages.login_signup
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            if (!IsPostBack)
+            {
+                BindStateList();
+            }
             
         }
+
         public string constr = ConfigurationManager.ConnectionStrings["mycon"].ConnectionString;
         public SqlDataAdapter sda;
         public SqlCommand cmd;
+
+        protected void BindStateList()
+        {
+            string connString = ConfigurationManager.ConnectionStrings["mycon"].ConnectionString;
+
+            using (SqlConnection con = new SqlConnection(connString))
+            {
+                SqlCommand cmd = new SqlCommand("Display_State", con);
+                cmd.CommandType = CommandType.StoredProcedure;
+
+                con.Open();
+                SqlDataReader dr = cmd.ExecuteReader();
+
+                StateList.DataSource = dr;
+                StateList.DataTextField = "State_Name";
+                StateList.DataValueField = "State_ID";
+                StateList.DataBind();
+            }
+            StateList.Items.Insert(0, new ListItem("Select State", ""));
+        }
+
+        protected void StateList_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+            string connString = ConfigurationManager.ConnectionStrings["mycon"].ConnectionString;
+
+            using (SqlConnection con = new SqlConnection(connString))
+            {
+                if (StateList.SelectedIndex <= 0)
+                {
+                    return; // Do NOT call SP when no state is selected
+                }
+                SqlCommand cmd = new SqlCommand("Display_City", con);
+                cmd.CommandType = CommandType.StoredProcedure;
+
+                cmd.Parameters.AddWithValue("@State_Name", StateList.SelectedItem.Text);
+
+                con.Open();
+                SqlDataReader dr = cmd.ExecuteReader();
+                CityList.DataSource = dr;
+                CityList.DataTextField = "City_Name";
+                if (StateList.Text != "Select State")
+                {
+                    CityList.DataBind();
+                }
+
+            }
+            CityList.Items.Insert(0, new ListItem("Select City", ""));
+        }
 
         protected void Worker_Signup_Button_Click(object sender, EventArgs e)
         {
@@ -33,19 +88,44 @@ namespace HomeServiceFinder.Pages.login_signup
             String age = Worker_Age_TextBox.Text;
             //String gender = Worker_Gender_DropDown.SelectedValue;
             String address = Worker_Address_TextBox.Text;
+            String ShopAddress = Worker_ShopAddress_TextBox.Text;
             String service = Worker_ServiceType_DropDown.SelectedValue;
             String experience = Worker_Experience_TextBox.Text;
             String password = Worker_Confirm_Password_TextBox.Text;
+            String city = CityList.SelectedItem.Text;
+            String minPrice = Worker_MinimumPrice_TextBox.Text;
 
-            cmd.CommandText = "Insert_User_Details";
-            cmd.CommandType = CommandType.StoredProcedure;
-            cmd.Parameters.AddWithValue('@User_Name', name);
-            cmd.Parameters.AddWithValue('@User_EmailID', email); ;
-            cmd.Parameters.AddWithValue('@User_Address', name);
-            cmd.Parameters.AddWithValue('@User_ContactNo', name);
-            cmd.Parameters.AddWithValue('@User_Role', name);
-            cmd.Parameters.AddWithValue('@City_ID', name);
+            //cmd.CommandText = "Insert_Worker_Details";
+            //cmd.CommandType = CommandType.StoredProcedure;
+            //cmd.Parameters.AddWithValue("@User_Name", name);
+            //cmd.Parameters.AddWithValue("@User_EmailID", email);
+            //cmd.Parameters.AddWithValue("@User_Address", address);
+            //cmd.Parameters.AddWithValue("@User_ContactNo", phone);
+            //cmd.Parameters.AddWithValue("@User_Password", password);
+            //cmd.Parameters.AddWithValue("@User_Role", "Worker");
+            //cmd.Parameters.AddWithValue("@City_Name", city);
 
+            //cmd.Parameters.AddWithValue("@SP_Age", Convert.ToInt32(age));
+            //cmd.Parameters.AddWithValue("@SP_ShopAddress", ShopAddress);
+            //cmd.Parameters.AddWithValue("@SP_Service",service);
+            //cmd.Parameters.AddWithValue("@SP_Experience", Convert.ToInt32(experience));
+            //cmd.Parameters.AddWithValue("@SP_MinimumPrice", Convert.ToInt32(minPrice));
+            //cmd.Parameters.AddWithValue("@SP_AverageRating", Convert.ToInt32("0"));
+
+
+            //int result = cmd.ExecuteNonQuery();
+            int result = 1;
+            if (result > 0)
+            {
+                Response.Write("<script>alert('Data inserted successfully');</script>");
+            }
+            else
+            {
+                Response.Write("<script>alert('Data not inserted successfully');</script>");
+            }
+
+            cmd.Connection.Close();
+            cmd.Connection.Dispose();
 
         }
     }
